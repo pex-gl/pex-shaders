7f02812b74658f8473d002802fb93a1998433d53
Damien Seguin <dmnsgn@gmail.com>
2022-06-06 17:09:44 +0100

    fix: post-processing path typos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export * as chunks from "./shaders/chunks/index.js";
 export * as pipeline from "./shaders/pipeline/index.js";
-export * as postProcessing from "shaders/post-rocessing/index.js";
+export * as postProcessing from "./shaders/post-processing/index.js";
 export * as reflectionProbe from "./shaders/reflection-probe/index.js";
 export * as skybox from "./shaders/skybox/index.js";

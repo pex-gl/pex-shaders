5ebc2459f589a1f349107270480124b46d98f9c0
Damien Seguin <dmnsgn@gmail.com>
2022-06-06 17:18:05 +0100

    fix: export default as instead of * as

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-export * as chunks from "./shaders/chunks/index.js";
-export * as pipeline from "./shaders/pipeline/index.js";
-export * as postProcessing from "./shaders/post-processing/index.js";
-export * as reflectionProbe from "./shaders/reflection-probe/index.js";
-export * as skybox from "./shaders/skybox/index.js";
+export { default as chunks } from "./shaders/chunks/index.js";
+export { default as pipeline } from "./shaders/pipeline/index.js";
+export { default as postProcessing } from "./shaders/post-processing/index.js";
+export { default as reflectionProbe } from "./shaders/reflection-probe/index.js";
+export { default as skybox } from "./shaders/skybox/index.js";

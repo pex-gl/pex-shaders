4c15105d9ff3ff4a0e7fc0150d0c0e7b13dd324c
Damien Seguin <dmnsgn@gmail.com>
2023-10-16 16:24:27 +0100

    refactor: rename segment to line and material to standard

--- shaders/pipeline/index.js
+++ shaders/pipeline/index.js
@@ -3,12 +3,12 @@ import blitFrag from "./blit.frag.js";
 import depthPassVert from "./depth-pass.vert.js";
 import depthPassFrag from "./depth-pass.frag.js";
 import depthPrePassFrag from "./depth-pre-pass.frag.js";
-import materialFrag from "./material.frag.js";
-import materialVert from "./material.vert.js";
+import standardFrag from "./standard.frag.js";
+import standardVert from "./standard.vert.js";
 import basicFrag from "./basic.frag.js";
 import basicVert from "./basic.vert.js";
-import segmentFrag from "./segment.frag.js";
-import segmentVert from "./segment.vert.js";
+import lineFrag from "./line.frag.js";
+import lineVert from "./line.vert.js";
 import overlayFrag from "./overlay.frag.js";
 import overlayVert from "./overlay.vert.js";
 import helperFrag from "./helper.frag.js";
@@ -21,9 +21,9 @@ export default {
   blit: { vert: blitVert, frag: blitFrag },
   depthPass: { vert: depthPassVert, frag: depthPassFrag },
   depthPrePass: { depthPassVert, frag: depthPrePassFrag },
-  material: { vert: materialVert, frag: materialFrag },
+  standard: { vert: standardVert, frag: standardFrag },
   basic: { vert: basicVert, frag: basicFrag },
-  segment: { vert: segmentVert, frag: segmentFrag },
+  line: { vert: lineVert, frag: lineFrag },
   overlay: { vert: overlayVert, frag: overlayFrag },
   helper: { vert: helperVert, frag: helperFrag },
   error: { vert: errorVert, frag: errorFrag },

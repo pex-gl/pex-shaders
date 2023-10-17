b59fd25a912974ad4aa0718daa6202e4d9f5266e
Damien Seguin <dmnsgn@gmail.com>
2023-10-17 17:09:26 +0100

    feat: add positionView divide for relative line width (wip)
    - still resolution dependent (resize height to test)

--- shaders/pipeline/line.vert.js
+++ shaders/pipeline/line.vert.js
@@ -9,36 +9,52 @@ attribute vec3 aPointB;
 attribute vec4 aColorA;
 attribute vec4 aColorB;
 
+#ifdef USE_INSTANCED_LINE_WIDTH
+attribute vec2 aLineWidth;
+#endif
+
 uniform mat4 uProjectionMatrix;
 uniform mat4 uViewMatrix;
 uniform mat4 uModelMatrix;
 
 uniform float uLineWidth;
 uniform vec2 uResolution;
-uniform float uLineZOffset;
 
 varying vec4 vColor;
 
 #define HOOK_VERT_DECLARATIONS_END
 
 void main() {
-  vec4 clip0 = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPointA, 1.0);
-  vec4 clip1 = uProjectionMatrix * uViewMatrix * uModelMatrix * vec4(aPointB, 1.0);
-  vec2 screen0 = uResolution * (0.5 * clip0.xy/clip0.w + 0.5);
-  vec2 screen1 = uResolution * (0.5 * clip1.xy/clip1.w + 0.5);
-
-  vec2 xBasis = normalize(screen1 - screen0);
-  vec2 yBasis = vec2(-xBasis.y, xBasis.x);
-  vec2 pt0 = screen0 + aColorA.a * uLineWidth * (aPosition.x * xBasis + aPosition.y * yBasis);
-  vec2 pt1 = screen1 + aColorA.a * uLineWidth * (aPosition.x * xBasis + aPosition.y * yBasis);
-  vec2 pt = mix(pt0, pt1, aPosition.z);
-  vec4 clip = mix(clip0, clip1, aPosition.z);
   vColor = mix(aColorA, aColorB, aPosition.z);
-
-  gl_Position = vec4(clip.w * ((2.0 * pt) / uResolution - 1.0), clip.z + uLineZOffset, clip.w);
 
   if (length(aPointA) == 0.0 || length(aPointB) == 0.0) {
     gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
+  } else {
+    vec4 positionViewA = uViewMatrix * uModelMatrix * vec4(aPointA, 1.0);
+    vec4 positionViewB = uViewMatrix * uModelMatrix * vec4(aPointB, 1.0);
+
+    vec4 clip0 = uProjectionMatrix * positionViewA;
+    vec4 clip1 = uProjectionMatrix * positionViewB;
+
+    vec2 screen0 = uResolution * (0.5 * clip0.xy / clip0.w + 0.5);
+    vec2 screen1 = uResolution * (0.5 * clip1.xy / clip1.w + 0.5);
+
+    vec2 xBasis = normalize(screen1 - screen0);
+    vec2 yBasis = vec2(-xBasis.y, xBasis.x);
+
+    vec2 width = uLineWidth * (aPosition.x * xBasis + aPosition.y * yBasis);
+
+    #ifdef USE_INSTANCED_LINE_WIDTH
+      width *= aLineWidth;
+    #endif
+
+    // TODO: it is still resolution dependent
+    vec2 pt0 = screen0 + (aColorA.a * width) / -positionViewA.z;
+    vec2 pt1 = screen1 + (aColorB.a * width) / -positionViewB.z;
+    vec2 pt = mix(pt0, pt1, aPosition.z);
+    vec4 clip = mix(clip0, clip1, aPosition.z);
+
+    gl_Position = vec4(clip.w * ((2.0 * pt) / uResolution - 1.0), clip.z, clip.w);
   }
 
   #define HOOK_VERT_END

c53dfd28be79e0ac781abc68ef492b50c1c83082
Damien Seguin <dmnsgn@gmail.com>
2024-05-03 14:25:56 +0100

    feat: remove reflectance in favor of ior

--- shaders/chunks/specular.glsl.js
+++ shaders/chunks/specular.glsl.js
@@ -1,15 +1,11 @@
 export default /* glsl */ `
-#ifdef USE_IOR
-  uniform float uIor;
+uniform float uIor;
 
-  void getIor(inout PBRData data) {
-    data.ior = uIor;
-  }
-#else
-  void getIor(inout PBRData data) {
-    data.ior = 1.5;
-  }
-#endif
+const float OUTSIDE_IOR = 1.0; // Air
+
+void getIor(inout PBRData data) {
+  data.ior = uIor;
+}
 
 #if defined(USE_SPECULAR) && !defined(USE_SPECULAR_GLOSSINESS_WORKFLOW)
   uniform float uSpecular;
@@ -58,10 +54,9 @@ export default /* glsl */ `
     // https://github.com/KhronosGroup/glTF/tree/main/extensions/2.0/Khronos/KHR_materials_specular#implementation
     // dielectricSpecularF0 = min(((ior - outside_ior) / (ior + outside_ior))^2 * specularColorFactor * specularColorTexture.rgb, float3(1.0)) * specularFactor * specularTexture.a
     // dielectricSpecularF90 = specularFactor * specularTexture.a
-    float outside_ior = 1.0;
     data.f0 = mix(
       min(
-        pow((data.ior - outside_ior) / (data.ior + outside_ior), 2.0) * specularColor,
+        pow((data.ior - OUTSIDE_IOR) / (data.ior + OUTSIDE_IOR), 2.0) * specularColor,
         vec3(1.0)
       ) * specularStrength,
       data.baseColor.rgb,
@@ -70,13 +65,13 @@ export default /* glsl */ `
     data.f90 = mix(vec3(specularStrength), vec3(1.0), data.metallic);
   }
 #else
-  uniform float uReflectance;
-
   void getSpecular(inout PBRData data) {
     // Compute F0 for both dielectric and metallic materials
-    data.f0 = 0.16 * uReflectance * uReflectance * (1.0 - data.metallic) + data.baseColor.rgb * data.metallic;
-    // TODO: is it equivalent
-    // data.f0 = mix(vec3(0.16 * uReflectance * uReflectance), data.diffuseColor.rgb, data.metallic);
+    data.f0 = mix(
+      vec3(pow((data.ior - OUTSIDE_IOR) /  (data.ior + OUTSIDE_IOR), 2.0)),
+      data.baseColor.rgb,
+      data.metallic
+    );
     data.f90 = vec3(1.0);
   }
 #endif

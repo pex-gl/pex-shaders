c0a98f628573e57de4f31e33461d15cff159c534
Damien Seguin <dmnsgn@gmail.com>
2025-09-01 11:39:18 +0100

    feat: split post-processing combine and final

--- shaders/post-processing/combine.frag.js
+++ shaders/post-processing/combine.frag.js
@@ -38,27 +38,12 @@ ${Object.values(glslToneMap).join("\n")}
 ${SHADERS.math.max3}
 ${SHADERS.reversibleToneMap}
 
-#if defined(USE_AA) || defined(USE_FILM_GRAIN)
-  ${SHADERS.luma}
-#endif
-
 #ifdef USE_FOG
   uniform float uFogStart;
   uniform vec3 uSunPosition;
 
   ${SHADERS.depthPosition}
   ${SHADERS.fog}
-#endif
-
-#ifdef USE_AA
-  // FXAA blends anything that has high enough contrast. It helps mitigate fireflies but will blur small details.
-  // - 1.00: upper limit (softer)
-  // - 0.75: default amount of filtering
-  // - 0.50: lower limit (sharper, less sub-pixel aliasing removal)
-  // - 0.25: almost off
-  // - 0.00: completely off
-  uniform float uSubPixelQuality;
-  ${SHADERS.fxaa}
 #endif
 
 #ifdef USE_SSAO
@@ -71,20 +56,6 @@ ${SHADERS.reversibleToneMap}
 #ifdef USE_BLOOM
   uniform sampler2D uBloomTexture;
   uniform float uBloomIntensity;
-#endif
-
-#ifdef USE_FILM_GRAIN
-  uniform float uFilmGrainSize;
-  uniform float uFilmGrainIntensity;
-  uniform float uFilmGrainColorIntensity;
-  uniform float uFilmGrainLuminanceIntensity;
-  uniform float uFilmGrainSpeed;
-
-  ${SHADERS.noise.common}
-  ${SHADERS.noise.simplex}
-  ${SHADERS.noise.perlin}
-  ${SHADERS.math.random}
-  ${SHADERS.filmGrain}
 #endif
 
 #ifdef USE_LUT
@@ -111,47 +82,13 @@ ${SHADERS.reversibleToneMap}
   ${SHADERS.vignette}
 #endif
 
-uniform float uOpacity;
-
 varying vec2 vTexCoord0;
-
-#if defined(USE_AA)
-  varying vec2 vTexCoord0LeftUp;
-  varying vec2 vTexCoord0RightUp;
-  varying vec2 vTexCoord0LeftDown;
-  varying vec2 vTexCoord0RightDown;
-  varying vec2 vTexCoord0Down;
-  varying vec2 vTexCoord0Up;
-  varying vec2 vTexCoord0Left;
-  varying vec2 vTexCoord0Right;
-#endif
 
 void main() {
   vec4 color = vec4(0.0);
 
   vec2 uv = vTexCoord0;
-
-  // Anti-aliasing
-  #ifdef USE_AA
-    color = fxaa(
-      uTexture,
-      uv,
-      vTexCoord0LeftUp,
-      vTexCoord0RightUp,
-      vTexCoord0LeftDown,
-      vTexCoord0RightDown,
-      vTexCoord0Down,
-      vTexCoord0Up,
-      vTexCoord0Left,
-      vTexCoord0Right,
-      uTexelSize,
-      uSubPixelQuality
-    );
-  #else
-    color = texture2D(uTexture, uv);
-  #endif
-
-  // color = decode(color, uTextureEncoding);
+  color = texture2D(uTexture, uv);
 
   // HDR effects
   #ifdef USE_FOG
@@ -186,19 +123,6 @@ void main() {
     color.rgb = vignette(color.rgb, uv, uVignetteRadius, uVignetteIntensity);
   #endif
 
-  #ifdef USE_FILM_GRAIN
-    color.rgb = filmGrain(
-      color.rgb,
-      uv,
-      uViewportSize,
-      uFilmGrainSize,
-      uFilmGrainIntensity,
-      uFilmGrainColorIntensity,
-      uFilmGrainLuminanceIntensity,
-      floor(uTime * uFilmGrainSpeed * 60.0)
-    );
-  #endif
-
   #ifdef USE_LUT
     color.rgb = lut(vec4(color.rgb, 1.0), uLUTTexture, uLUTTextureSize).rgb;
   #endif
@@ -210,7 +134,6 @@ void main() {
   #endif
 
   gl_FragColor = color;
-  gl_FragColor.a *= uOpacity;
 
   ${SHADERS.output.assignment}
 }
---
+++ shaders/post-processing/final.frag.js
@@ -0,0 +1,108 @@
+import * as glslToneMap from "glsl-tone-map";
+
+import * as SHADERS from "../chunks/index.js";
+
+/**
+ * @alias module:postProcessing.postProcessing.frag
+ * @type {string}
+ */
+export default /* glsl */ `precision highp float;
+
+${SHADERS.output.frag}
+
+// Variables
+uniform sampler2D uTexture;
+uniform vec2 uViewportSize;
+uniform vec2 uTexelSize;
+uniform float uTime;
+
+// Includes
+${SHADERS.math.saturate}
+
+#if defined(USE_AA) || defined(USE_FILM_GRAIN)
+  ${SHADERS.luma}
+#endif
+
+#ifdef USE_AA
+  // FXAA blends anything that has high enough contrast. It helps mitigate fireflies but will blur small details.
+  // - 1.00: upper limit (softer)
+  // - 0.75: default amount of filtering
+  // - 0.50: lower limit (sharper, less sub-pixel aliasing removal)
+  // - 0.25: almost off
+  // - 0.00: completely off
+  uniform float uSubPixelQuality;
+  ${SHADERS.fxaa}
+#endif
+
+#ifdef USE_FILM_GRAIN
+  uniform float uFilmGrainSize;
+  uniform float uFilmGrainIntensity;
+  uniform float uFilmGrainColorIntensity;
+  uniform float uFilmGrainLuminanceIntensity;
+  uniform float uFilmGrainSpeed;
+
+  ${SHADERS.noise.common}
+  ${SHADERS.noise.simplex}
+  ${SHADERS.noise.perlin}
+  ${SHADERS.math.random}
+  ${SHADERS.filmGrain}
+#endif
+
+uniform float uOpacity;
+
+varying vec2 vTexCoord0;
+
+#if defined(USE_AA)
+  varying vec2 vTexCoord0LeftUp;
+  varying vec2 vTexCoord0RightUp;
+  varying vec2 vTexCoord0LeftDown;
+  varying vec2 vTexCoord0RightDown;
+  varying vec2 vTexCoord0Down;
+  varying vec2 vTexCoord0Up;
+  varying vec2 vTexCoord0Left;
+  varying vec2 vTexCoord0Right;
+#endif
+
+void main() {
+  vec4 color = vec4(0.0);
+
+  vec2 uv = vTexCoord0;
+
+  #ifdef USE_AA
+    color = fxaa(
+      uTexture,
+      uv,
+      vTexCoord0LeftUp,
+      vTexCoord0RightUp,
+      vTexCoord0LeftDown,
+      vTexCoord0RightDown,
+      vTexCoord0Down,
+      vTexCoord0Up,
+      vTexCoord0Left,
+      vTexCoord0Right,
+      uTexelSize,
+      uSubPixelQuality
+    );
+  #else
+    color = texture2D(uTexture, uv);
+  #endif
+
+  #ifdef USE_FILM_GRAIN
+    color.rgb = filmGrain(
+      color.rgb,
+      uv,
+      uViewportSize,
+      uFilmGrainSize,
+      uFilmGrainIntensity,
+      uFilmGrainColorIntensity,
+      uFilmGrainLuminanceIntensity,
+      floor(uTime * uFilmGrainSpeed * 60.0)
+    );
+  #endif
+
+  gl_FragColor = color;
+  gl_FragColor.a *= uOpacity;
+
+  ${SHADERS.output.assignment}
+}
+`;

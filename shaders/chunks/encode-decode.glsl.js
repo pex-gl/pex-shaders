3bac8ee156083ae170c3702e22ec7957ac869a92
Damien Seguin <dmnsgn@gmail.com>
2026-04-02 14:24:34 +0100

    feat(main): use the correct srgb transfer function
    - match pex-color
    
    Closes #29

--- shaders/chunks/encode-decode.glsl.js
+++ shaders/chunks/encode-decode.glsl.js
@@ -3,38 +3,37 @@ export default /* glsl */ `
 #define GAMMA 2
 #define SRGB 3
 
-const float gamma = 2.2;
+float linearToSrgb(float c) {
+  return (c <= 0.0031308) ? 12.92 * c : 1.055 * pow(c, 1.0 / 2.4) - 0.055;
+}
+float srgbToLinear(float c) {
+  return (c > 0.04045) ? pow((c + 0.055) / 1.055, 2.4) : c / 12.92;
+}
 
 // Linear
 float toLinear(float v) {
-  return pow(v, gamma);
+  return srgbToLinear(v);
 }
-
 vec2 toLinear(vec2 v) {
-  return pow(v, vec2(gamma));
+  return vec2(srgbToLinear(v.r), srgbToLinear(v.g));
 }
-
 vec3 toLinear(vec3 v) {
-  return pow(v, vec3(gamma));
+  return vec3(srgbToLinear(v.r), srgbToLinear(v.g), srgbToLinear(v.b));
 }
-
 vec4 toLinear(vec4 v) {
   return vec4(toLinear(v.rgb), v.a);
 }
 
 // Gamma
 float toGamma(float v) {
-  return pow(v, 1.0 / gamma);
+  return linearToSrgb(v);
 }
-
 vec2 toGamma(vec2 v) {
-  return pow(v, vec2(1.0 / gamma));
+  return vec2(linearToSrgb(v.r), linearToSrgb(v.g));
 }
-
 vec3 toGamma(vec3 v) {
-  return pow(v, vec3(1.0 / gamma));
+  return vec3(linearToSrgb(v.r), linearToSrgb(v.g), linearToSrgb(v.b));
 }
-
 vec4 toGamma(vec4 v) {
   return vec4(toGamma(v.rgb), v.a);
 }
